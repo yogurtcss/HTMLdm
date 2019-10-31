816a8f7833319808403474f5d87452713bef1230
yogurtcss <1806631955@qq.com>
2019-10-31 17:33:28 +0800

    upload

--- ReactDM/npmReact/gzhipin/gzhipin-client1/src/redux/action-type.js
+++ ReactDM/npmReact/gzhipin/gzhipin-client1/src/redux/action-type.js
@@ -2,4 +2,8 @@
 
 // authorization授权
 export const AUTH_SUCCESS = 'auth_success'; //注册及登陆成功
-export const ERROR_MSG = 'error_msg'; //错误提示信息
+export const ERROR_MSG = 'error_msg'; //请求前或请求后 的错误提示信息
+
+/* 为什么ERROR_MSG 不写成：AUTH_Failure或AUTH_ERROR (授权失败)呢？
+* 理由：前端验证(后面补充)
+*  */
