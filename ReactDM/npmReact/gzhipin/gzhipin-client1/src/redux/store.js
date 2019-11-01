607a9fcadd264a991beb15ed57bdc1d2ca306c9f
yogurtcss <1806631955@qq.com>
2019-11-01 13:20:32 +0800

    upload

--- ReactDM/npmReact/gzhipin/gzhipin-client1/src/redux/store.js
+++ ReactDM/npmReact/gzhipin/gzhipin-client1/src/redux/store.js
@@ -4,7 +4,7 @@ import {createStore,applyMiddleware} from 'redux';
 import thunk from 'redux-thunk'; //异步中间件
 import {composeWithDevTools} from 'redux-devtools-extension'; //扩展工具
 
-import reducers from './reducers.js';
+import reducers from './reducers.js'; //接收从reducers.js 的combineReducers产出的新对象newState，以存在此store中
 
 /* 向外暴露store对象：在外部直接 import store from '..这个/store.js'即可
 * 接收从reducers.js 的combineReducers产出的新对象newState，存在此store中
