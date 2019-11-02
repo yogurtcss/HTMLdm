889f79ab439772c34cf070f23722f183cb0a21cc
yogurtcss <1806631955@qq.com>
2019-11-02 10:33:57 +0800

    upload

--- ReactDM/npmReact/gzhipin/gzhipin-client1/src/containers/main/main.jsx
+++ ReactDM/npmReact/gzhipin/gzhipin-client1/src/containers/main/main.jsx
@@ -1,11 +1,23 @@
 //主界面路由组件
 
 import React, {Component} from 'react';
+import {Switch, Route} from 'react-router-dom';
+
+//laoban-info和dashen-info，都是Main路由下的二级路由
+import LaobanInfo from '../laoban-info/laoban-info.jsx';
+import DashenInfo from '../dashen-info/dashen-info.jsx';
 
 export default class Main extends Component{
     render(){
         return(
-            <div>Main</div>
+            //laoban-info和dashen-info，都是Main路由下的二级路由
+            <div>
+                <Switch>
+                    {/* 映射 这两个组件 为Main路由下的二级路由 */}
+                    <Route path='/laobaninfo' component={LaobanInfo} />
+                    <Route path='/dasheninfo' component={DashenInfo} />
+                </Switch>
+            </div>
         )
     }
 }
