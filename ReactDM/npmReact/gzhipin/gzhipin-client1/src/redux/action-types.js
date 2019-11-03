f12aab4e59b92e4dc18e39d3254caa2cb7a9ac7f
yogurtcss <1806631955@qq.com>
2019-11-03 10:13:14 +0800

    upload

--- ReactDM/npmReact/gzhipin/gzhipin-client1/src/redux/action-types.js
+++ ReactDM/npmReact/gzhipin/gzhipin-client1/src/redux/action-types.js
@@ -7,3 +7,5 @@ export const ERROR_MSG = 'error_msg'; //请求前或请求后 的错误提示信
 /* 为什么ERROR_MSG 不写成：AUTH_Failure或AUTH_ERROR (授权失败)呢？
 * 理由：前端验证(后面补充)
 *  */
+
+export const RECEIVE_USER = 'receive_user'; //接收 返回响应中的、更新完毕的用户信息
