820fd7e0cbd24c32a523642f98317d1256aaa677
yogurtcss <1806631955@qq.com>
2019-11-03 16:53:04 +0800

    upload

--- ReactDM/npmReact/gzhipin/gzhipin-client1/src/containers/main/main.jsx
+++ ReactDM/npmReact/gzhipin/gzhipin-client1/src/containers/main/main.jsx
@@ -1,14 +1,26 @@
 //主界面路由组件
 
 import React, {Component} from 'react';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
+import {connect} from 'react-redux';
 
 //laoban-info和dashen-info，都是Main路由下的二级路由
 import LaobanInfo from '../laoban-info/laoban-info.jsx';
 import DashenInfo from '../dashen-info/dashen-info.jsx';
 
-export default class Main extends Component{
+class Main extends Component{
     render(){
+        /* 某种特殊情况：正当用户登陆后，此用户的cookie信息不慎丢失了；
+        * 当此用户要更新信息(点击 保存 按钮时)，则将此用户强退(重定向)至 登陆界面
+        *
+        * 根据已登陆用户信息 cookie中的userid判断：
+        * 检查用户是否登陆，若未登陆，则自动重定向至登陆界面
+        *  */
+        const {user} = this.props;
+        if( !user._id ){
+            return( <Redirect to={'/login'} /> )
+        }
+
         return(
             //laoban-info和dashen-info，都是Main路由下的二级路由
             <div>
@@ -21,3 +33,7 @@ export default class Main extends Component{
         )
     }
 }
+
+export default connect(
+    state => ( {user:state.user} )
+)(Main);
